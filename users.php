6f459790c7cf4851dc2223a43ae280ddcf122fde
tjhamadeh <tjhamadeh@gmail.com>
2014-09-21 04:57:37 -0400

    users.php
    looks fine

--- users.php
+++ users.php
@@ -6,7 +6,8 @@ $mysqlserver = "sql2.njit.edu";
 $mysqldb = "ll37";
 
 $mysqli = mysqli_connect($mysqlserver,$mysqlusername, $mysqlpassword, $mysqldb);
-
+$myquery1="SELECT Id, Email, Rank FROM Users WHERE uCode='".$_COOKIE["mycode"]."';";
+//echo $myquery1."<br>";
 $query = mysqli_query($mysqli, "SELECT Id, Email, Rank FROM Users WHERE uCode='".$_COOKIE["mycode"]."';");
 $row = $query->fetch_assoc();
 $rank = $row['Rank'];
@@ -20,7 +21,9 @@ if($username!="")
     $checker = 0;
     while(isset($_POST[$checker]))
     {
-        $mysqli->query("UPDATE TABLE Users SET Rank=".$_POST["rank".$checker]." WHERE Id = ".$_POST[$checker].";");
+        $myquery = "UPDATE Users SET Rank=".$_POST["rank".$checker]." WHERE Id = ".$_POST[$checker].";";
+        $mysqli->query($myquery);
+        //echo $myquery."<br>";
         $checker++;
     }
 }
@@ -30,16 +33,16 @@ if ($rank == 99)
     echo "<form action='./users.php' method='post'>";
     echo "<table>";
     echo "<tr><td>User</td><td>Rank</td></tr>";
-    $query = mysqli_query($mysqli, "SELECT Email, Rank FROM Users;");
+    $query = mysqli_query($mysqli, "SELECT Id, Email, Rank FROM Users;");
     $cnt = 0;
     while ($row = $query->fetch_assoc())
     {
-        echo "<tr><td>".$row['Email']."</td><td><input type='hidden' name='".$cnt."' value='".$row["Id"]."'><select name='rank".$cnt."'>
-            <option value='' ></option><option value='1' ";
+        echo "<tr><td>".$row['Email']."</td><td><input type='hidden' name='".$cnt."' value='".$row['Id']."'><select name='rank".$cnt."'>
+            <option value='1' ></option><option value='1' ";
         if($row["Rank"] == 1) echo "selected";
             echo">Member</option><option value='99' ";
         if($row["Rank"] == 99) echo "selected";
-        echo">Admin</option></select></td></tr>";
+            echo ">Admin</option></select></td></tr>";
         $cnt++;
     }
     echo "<tr><td><input type='submit' name='submit' value='submit'></td></tr>";
